12081147917f6060957b4fb184ac885855b9d358
EmreAnliacik <emreanliacik@hotmail.com>
2025-05-21 01:33:11 +0200

    Operators

---
+++ _03_Operators.js
@@ -0,0 +1,71 @@
+// Arithmetic Operators
+
+// Addition
+
+let sum = 5 + 3;
+console.log(sum);
+
+// Substraction
+ let subs = 5-3;
+ console.log(subs);
+
+
+//  Multiplication
+let multi = 5 * 3;
+console.log(multi);
+
+// Division
+let divis = 5 / 3;
+console.log(divis);
+
+// Modulus : Bolumden kalani verir
+ let mod = 5 % 2;
+ console.log(mod);
+
+
+// Exponentiation :Uslu sayi seklinde
+let exp = 2 ** 8;
+console.log(exp);
+
+
+//      Assigment Operators
+
+let d = 5;
+d += 3;
+d -= 2;
+d **= 3;
+d /= 6;
+
+console.log(d)
+
+
+
+//           INCREMENT / DECREMENT OPERATORS
+
+let a =10;
+console.log(++a);
+console.log(a)
+console.log(--a);
+console.log(a)
+
+console.log(a++); //10
+console.log(a) //11
+console.log(a--); //11
+console.log(a)  //10
+
+//               COMPARISON OPERATORS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
